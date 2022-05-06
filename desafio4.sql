f3de2b763029ffd4a496d11c41dcbcb877d5a4aa
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-05-06 19:26:42 -0300

    Desafio4: Implementando query para filtragem de usuarios ativos

---
+++ desafio4.sql
@@ -0,0 +1,7 @@
+SELECT usuarios.usuario AS usuario,
+IF(MAX(YEAR(historico.data_reproducao)) = 2021, 'Usuário ativo', 'Usuário inativo') AS condicao_usuario
+FROM SpotifyClone.usuarios as usuarios
+INNER JOIN SpotifyClone.historico_de_reproducoes AS historico
+ON usuarios.usuario_id = historico.usuario_id
+group by usuarios.usuario
+;
